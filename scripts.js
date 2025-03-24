a41db4f668a0250634299af794491ea605dff0c2
Manu <manuelguese01@gmail.com>
2025-03-24 18:20:34 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function handleButtonClick() {
     setTimeout(function() {
-        window.location.href = 'https://google.com';
+        window.location.href = 'https://google.com'; // Sustituir por página principal
     }, 0); 
 }
